61b08247e8b6eba22f7b60f7196d9cf6222edeaf
Antoh1 <antotiro@gmail.com>
2022-09-27 19:48:41 +0300

    Added OE and Vendor Number fields

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "C:\Users\USER\Desktop\Modules\ProductFields",
+    'name': "ProductFields",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        This module adds more fields to the product display view""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Tirop",
+    'website': "http://www.ropesolutions.co.ke",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
@@ -20,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'stock'],
 
     # always loaded
     'data': [
--- models/models.py
+++ models/models.py
@@ -2,14 +2,8 @@
 
 from odoo import models, fields, api
 
-# class c:\users\user\desktop\modules\product_fields(models.Model):
-#     _name = 'c:\users\user\desktop\modules\product_fields.c:\users\user\desktop\modules\product_fields'
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    ppartno = fields.Char('OE Number')
+    mpartno = fields.Char('Vendor Number')
