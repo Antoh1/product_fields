1454432a5d2c069f524f948c038724dfc8d89970
Antoh1 <antotiro@gmail.com>
2022-10-14 16:15:41 +0300

    [UPDT] added inherited field to be overiden

--- models/models.py
+++ models/models.py
@@ -3,7 +3,9 @@
 from odoo import models, fields, api
 
 class ProductTemplate(models.Model):
+	#_name = 'product.template'
     _inherit = 'product.template'
 
     ppartno = fields.Char('OE Number')
     mpartno = fields.Char('Vendor Number')
+    default_code = fields.Char('Brand')
